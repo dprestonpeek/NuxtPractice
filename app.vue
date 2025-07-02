69d8c8c69969ef50559d8f2e8a298e09bc02e24c
Preston Peek <36671440+dprestonpeek@users.noreply.github.com>
2025-07-02 12:15:56 -0400

    Added shopping list page

--- app.vue
+++ app.vue
@@ -1,6 +1,29 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <header>
+      <nav>
+        <div>
+          <NuxtLink to="/">Home</NuxtLink> &nbsp;
+          <NuxtLink to="/shoppinglist">Shopping List</NuxtLink>
+        </div>
+        <br><br>
+      </nav>
+    </header>
+
+    <main>
+      <NuxtPage />
+    </main>
+
+    <footer>
+
+    </footer>
   </div>
 </template>
+
+<script setup>
+  //Global app logic
+</script>
+
+<style>
+  /*Global app styles */
+</style>
